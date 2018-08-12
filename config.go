55dc7e434f59ba80f94a2d34010a791b118a572d
Ravern Koh <ravernkoh@gmail.com>
2018-08-12 11:59:46 +0800

    Add type definitions

---
+++ config.go
@@ -0,0 +1,42 @@
+package mojo
+
+// Config contains configuration that defines how to parse certain objects.
+type Config struct {
+	Root ConfigCommand
+
+	// AllowMultipleFlags indicates whether combining multiple flags
+	// (e.g. ls -al) is allowed.
+	//
+	// If it is allowed, such flags will be parsed as multiple flags with
+	// the last one containing a value unless indicated otherwise by
+	// configuration. If it isn't allowed, they will be parsed as a single
+	// flag instead.
+	AllowMutipleFlags bool
+
+	// DisallowCombinedFlagValues indicates whether combining the flag and
+	// value together (e.g. --flag=value) is not allowed.
+	//
+	// If it isn't allowed, then these flags will be taken literally, with
+	// their name set to whatever the combined flag and value is.
+	DisallowCombinedFlagValues bool
+
+	// DisallowDoubleDash indicates whether the double dash (i.e. --) is
+	// not allowed.
+	//
+	// If it is allowed, it will be parsed as an argument. However, if it
+	// isn't allowed, then an error will occur since it will be treated
+	// as a flag without a name.
+	DisallowDoubleDash bool
+}
+
+// ConfigCommand contains configuration for a command.
+type ConfigCommand struct {
+	Commands []ConfigCommand
+	Flags    []ConfigFlag
+}
+
+// ConfigFlag contains configuration for a flag.
+type ConfigFlag struct {
+	Name string
+	Bool bool
+}
---
+++ object.go
@@ -0,0 +1,28 @@
+package mojo
+
+// Object represents a command, flag or argument.
+type Object interface {
+	object()
+}
+
+// ObjectCommand represents a command that has been parsed.
+type ObjectCommand struct {
+	Name string
+}
+
+func (*ObjectCommand) object() {}
+
+// ObjectFlag represents a flag that has been parsed.
+type ObjectFlag struct {
+	Name  string
+	Value string
+}
+
+func (*ObjectFlag) object() {}
+
+// ObjectArgument represents an argument that has been parsed.
+type ObjectArgument struct {
+	Value string
+}
+
+func (*ObjectArgument) object() {}
