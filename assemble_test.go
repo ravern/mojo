c75b91d9ef5c98bc5800468eea8b5c7115ac746a
Ravern Koh <ravernkoh@gmail.com>
2018-08-13 23:01:43 +0800

    Add unit tests for assembling and fix bug

--- assemble_test.go
+++ assemble_test.go
@@ -8,14 +8,13 @@ import (
 	"github.com/ravernkoh/mojo"
 )
 
-func TestParse(t *testing.T) {
+func TestObjects_Assemble(t *testing.T) {
 	type args struct {
-		conf mojo.Config
-		args []string
+		objs mojo.Objects
 	}
 
 	type rets struct {
-		objs mojo.Objects
+		args []string
 		err  error
 	}
 
@@ -27,78 +26,44 @@ func TestParse(t *testing.T) {
 		{
 			name: "Argument",
 			args: args{
-				conf: mojo.Config{
-					Root: mojo.ConfigCommand{
-						Name: "tldr",
-					},
-				},
-				args: []string{"tldr", "nmap"},
-			},
-			want: rets{
 				objs: []mojo.Object{
 					mojo.ObjectCommand{Name: "tldr"},
 					mojo.ObjectArgument{Value: "nmap"},
 				},
 			},
+			want: rets{
+				args: []string{"tldr", "nmap"},
+			},
 		},
 		{
 			name: "BoolFlagAndArgument",
 			args: args{
-				conf: mojo.Config{
-					Root: mojo.ConfigCommand{
-						Name: "tldr",
-						Flags: []mojo.ConfigFlag{
-							{
-								Name: "--verbose",
-								Bool: true,
-							},
-						},
-					},
-				},
-				args: []string{"tldr", "--verbose", "nmap"},
-			},
-			want: rets{
 				objs: []mojo.Object{
 					mojo.ObjectCommand{Name: "tldr"},
 					mojo.ObjectFlag{Name: "--verbose", Bool: true},
 					mojo.ObjectArgument{Value: "nmap"},
 				},
 			},
+			want: rets{
+				args: []string{"tldr", "--verbose", "nmap"},
+			},
 		},
 		{
 			name: "FlagAndArgument",
 			args: args{
-				conf: mojo.Config{
-					Root: mojo.ConfigCommand{
-						Name: "tldr",
-						Flags: []mojo.ConfigFlag{
-							{Name: "--level"},
-						},
-					},
-				},
-				args: []string{"tldr", "--level", "5", "nmap"},
-			},
-			want: rets{
 				objs: []mojo.Object{
 					mojo.ObjectCommand{Name: "tldr"},
 					mojo.ObjectFlag{Name: "--level", Value: "5"},
 					mojo.ObjectArgument{Value: "nmap"},
 				},
 			},
+			want: rets{
+				args: []string{"tldr", "--level", "5", "nmap"},
+			},
 		},
 		{
 			name: "MultipleFlagsAndArgument",
 			args: args{
-				conf: mojo.Config{
-					AllowMutipleFlags:      true,
-					AllowUnconfiguredFlags: true,
-					Root: mojo.ConfigCommand{
-						Name: "tldr",
-					},
-				},
-				args: []string{"tldr", "-vbl", "5", "nmap"},
-			},
-			want: rets{
 				objs: []mojo.Object{
 					mojo.ObjectCommand{Name: "tldr"},
 					mojo.ObjectFlag{Name: "-v", Bool: true, MultipleFlagsStart: true},
@@ -107,20 +72,13 @@ func TestParse(t *testing.T) {
 					mojo.ObjectArgument{Value: "nmap"},
 				},
 			},
+			want: rets{
+				args: []string{"tldr", "-vbl", "5", "nmap"},
+			},
 		},
 		{
 			name: "CombinedMultipleFlagValuesAndArgument",
 			args: args{
-				conf: mojo.Config{
-					AllowMutipleFlags:      true,
-					AllowUnconfiguredFlags: true,
-					Root: mojo.ConfigCommand{
-						Name: "tldr",
-					},
-				},
-				args: []string{"tldr", "-vl=5", "nmap"},
-			},
-			want: rets{
 				objs: []mojo.Object{
 					mojo.ObjectCommand{Name: "tldr"},
 					mojo.ObjectFlag{Name: "-v", Bool: true, MultipleFlagsStart: true},
@@ -128,73 +86,38 @@ func TestParse(t *testing.T) {
 					mojo.ObjectArgument{Value: "nmap"},
 				},
 			},
+			want: rets{
+				args: []string{"tldr", "-vl=5", "nmap"},
+			},
 		},
 		{
 			name: "SubcommandAndArgument",
 			args: args{
-				conf: mojo.Config{
-					Root: mojo.ConfigCommand{
-						Name: "tldr",
-						Commands: []mojo.ConfigCommand{
-							{Name: "add"},
-						},
-					},
-				},
-				args: []string{"tldr", "add", "nmap"},
-			},
-			want: rets{
 				objs: []mojo.Object{
 					mojo.ObjectCommand{Name: "tldr"},
 					mojo.ObjectCommand{Name: "add"},
 					mojo.ObjectArgument{Value: "nmap"},
 				},
 			},
+			want: rets{
+				args: []string{"tldr", "add", "nmap"},
+			},
 		},
 		{
 			name: "SubcommandOrArgument",
 			args: args{
-				conf: mojo.Config{
-					Root: mojo.ConfigCommand{
-						Name: "tldr",
-						Commands: []mojo.ConfigCommand{
-							{Name: "add"},
-						},
-					},
-				},
-				args: []string{"tldr", "nmap"},
-			},
-			want: rets{
 				objs: []mojo.Object{
 					mojo.ObjectCommand{Name: "tldr"},
 					mojo.ObjectArgument{Value: "nmap"},
 				},
 			},
+			want: rets{
+				args: []string{"tldr", "nmap"},
+			},
 		},
 		{
 			name: "SubcommandAndFlagAndArgument",
 			args: args{
-				conf: mojo.Config{
-					Root: mojo.ConfigCommand{
-						Name: "tldr",
-						Commands: []mojo.ConfigCommand{
-							{
-								Name: "add",
-								Flags: []mojo.ConfigFlag{
-									{Name: "--level"},
-								},
-							},
-						},
-						Flags: []mojo.ConfigFlag{
-							{
-								Name: "--level",
-								Bool: true,
-							},
-						},
-					},
-				},
-				args: []string{"tldr", "add", "--level", "5", "nmap"},
-			},
-			want: rets{
 				objs: []mojo.Object{
 					mojo.ObjectCommand{Name: "tldr"},
 					mojo.ObjectCommand{Name: "add"},
@@ -202,18 +125,21 @@ func TestParse(t *testing.T) {
 					mojo.ObjectArgument{Value: "nmap"},
 				},
 			},
+			want: rets{
+				args: []string{"tldr", "add", "--level", "5", "nmap"},
+			},
 		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			var got rets
-			got.objs, got.err = mojo.Parse(test.args.conf, test.args.args)
+			got.args, got.err = test.args.objs.Assemble()
 			if fmt.Sprintf("%v", got.err) != fmt.Sprintf("%v", test.want.err) {
 				t.Errorf("want err %v, got err %v", test.want.err, got.err)
 				return
 			}
-			if !reflect.DeepEqual(got.objs, test.want.objs) {
-				t.Errorf("want objs %v, got objs %v", test.want.objs, got.objs)
+			if !reflect.DeepEqual(got.args, test.want.args) {
+				t.Errorf("want objs %v, got objs %v", test.want.args, got.args)
 			}
 		})
 	}
